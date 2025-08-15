0d40b3d656bbc1eb985f5e63693b0eca69d154b1
jhalakgupta12 <jhalakgupta12304@gmail.com>
2025-08-15 13:07:40 +0000

    Tailwind Working

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import TailwindTest from './components/TailwindTest'
 import Navbar from "./components/Navbar";
 
